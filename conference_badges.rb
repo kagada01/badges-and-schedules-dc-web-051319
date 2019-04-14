9ef3515cae233b5e46958876fafa3a6c024f8d36
Dan Kagan <dankagan7@gmail.com>
2019-04-14 03:07:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,6 +28,6 @@ def printer(attendees)
     batch_badge_creator(attendees).each do |badge|
       puts badge
     assign_rooms(attendees).each do |badge|
-      puts assign_rooms(attendees).shift
+      puts badge
   end
 end
